fb367ab902d7033ff688f720d2d7506f750acb61
Atin Mathur <atin.mathur.10@gmail.com>
2017-08-05 18:18:07 +0530

    Added the link to my profile

--- src/screens/MenuList.js
+++ src/screens/MenuList.js
@@ -6,58 +6,70 @@ import {
 } from 'react-native';
 import { List, ListItem } from 'react-native-elements';
 
-import { users } from '../config/data';
-
-const list = [
-  {
-    title: 'My Profile',
-    icon: 'ios-person-outline',
-    type: 'ionicon'
-  },
-  {
-    title: 'My Appointments',
-    icon: 'ios-calendar-outline',
-    type: 'ionicon'
-  },
-  {
-    title: 'Help Center',
-    icon: 'ios-help-circle-outline',
-    type: 'ionicon'
-  },
-  {
-    title: 'Terms & Policies',
-    icon: 'ios-paper-outline',
-    type: 'ionicon'
-  },
-  {
-    title: 'Report a Problem',
-    icon: 'ios-bug-outline',
-    type: 'ionicon'
-  },
-  {
-    title: 'About',
-    icon: 'ios-information-circle-outline',
-    type: 'ionicon'
-  },
-  {
-    title: 'Logout',
-    icon: 'ios-log-out-outline',
-    type: 'ionicon'
-  }
-];
+import { users, me } from '../config/data';
 
 class MenuList extends Component {
+  toMyProfile = (user) => {
+    this.props.navigation.navigate('UserProfile', { ...user });
+  };
+
+  componentWillMount() {
+    this.list = [
+      {
+        title: 'My Profile',
+        icon: 'ios-person-outline',
+        type: 'ionicon',
+        onPress: () => this.toMyProfile(me)
+      },
+      {
+        title: 'My Appointments',
+        icon: 'ios-calendar-outline',
+        type: 'ionicon',
+        onPress: null
+      },
+      {
+        title: 'Help Center',
+        icon: 'ios-help-circle-outline',
+        type: 'ionicon',
+        onPress: null
+      },
+      {
+        title: 'Terms & Policies',
+        icon: 'ios-paper-outline',
+        type: 'ionicon',
+        onPress: null
+      },
+      {
+        title: 'Report a Problem',
+        icon: 'ios-bug-outline',
+        type: 'ionicon',
+        onPress: null
+      },
+      {
+        title: 'About',
+        icon: 'ios-information-circle-outline',
+        type: 'ionicon',
+        onPress: null
+      },
+      {
+        title: 'Logout',
+        icon: 'ios-log-out-outline',
+        type: 'ionicon',
+        onPress: null
+      }
+    ];
+  }
 
   render() {
     return (
       <ScrollView>
         <List containerStyle={{marginTop: 0}}>
-          {list.map((item) => (
+          {this.list.map((item) => (
             <ListItem
               key={item.title}
               title={item.title}
               leftIcon={{name: item.icon, type: item.type}}
-              // onPress={() => this.onLearnMore(user)}
+              onPress={item.onPress}
             />
           ))}
         </List>
