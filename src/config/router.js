ee5c26068db1ced870d687b34715eae8309eb9e2
Atin Mathur <atin.mathur.10@gmail.com>
2017-08-10 13:11:59 +0530

    Important commit - Modified the Navigator hierarchy

--- src/config/router.js
+++ src/config/router.js
@@ -16,152 +16,78 @@ import ShareUpdate from '../screens/ShareUpdate';
 // Convert a string to TitleCase (react native -> React Native, REACT NATIVE -> React Native, reAct nATive -> React Native)
 import { toTitleCase } from '../helpers';
 
-// Stack Navigator for the SignUp page
-export const SignUpStack = StackNavigator({
-  SignUp: {
-    screen: SignUp,
-    navigationOptions: {
-      title: 'SignUp',
-    },
-  },
-},);
-
-// Stack navigator for Friends
-export const FriendListStack = StackNavigator({
-  FriendList: {
-    screen: FriendList,
-    navigationOptions: {
-      title: 'Friends',
-    }
-  },
-}, {
-  mode: 'card',
-  headerMode: 'none',
-});
-
-// Stack navigator for User Profile
-export const UserProfileStack = StackNavigator({
-  UserProfile: {
-    screen: UserProfile,
-    navigationOptions: ({ navigation }) => ({
-      title: `${toTitleCase(navigation.state.params.name.first)} ${toTitleCase(navigation.state.params.name.last)}`,
-    }),
-  },
-}, {
-  mode: 'card',
-  headerMode: 'none',
-});
-
-// Stack navigator for 1. Home tab
-export const HomeStack = StackNavigator({
+// Tab Navigator with 4 tabs (Home, Friends, Notifications, Menu)
+export const Tabs = TabNavigator({
   Home: {
     screen: Home,
     navigationOptions: {
       title: 'Home',
-    },
-  }, Share: {
-    screen: ShareUpdate,
-    navigationOptions: {
-      title: 'Share',
-    },
-  },
-},);
-
-// Stack navigator for 2. FriendList tab
-export const FriendStack = StackNavigator({
-  Friend: {
-    screen: FriendListStack,
-    navigationOptions: {
-      title: 'My Friends',
-    },
-  }, FriendProfile : {
-      screen: UserProfileStack,
-  }
-},);
-
-// Stack navigator for 3. NotificationList tab
-export const NotificationStack = StackNavigator({
-  Notifications: {
-    screen: NotificationList,
-    navigationOptions: {
-      title: 'Notifications',
-    },
-  },
-},);
-
-// Stack navigator for 4. MenuList tab
-export const MenuStack = StackNavigator({
-  Notifications: {
-    screen: MenuList,
-    navigationOptions: {
-      title: 'Menu',
-    },
-  }, Policy: {
-    screen: Policy,
-    navigationOptions: {
-      title: 'Privacy Policy',
-    },
-  }, MyProfile: {
-    screen: UserProfileStack,
-    navigationOptions: {
-      title: 'My Profile',
-      headerRight: <Button title='Edit Profile' />,
-    }
-  }
-},);
-
-// Tab Navigator with 4 tabs (Home, Friends, Notifications, Menu)
-export const Tabs = TabNavigator({
-  Home: {
-    screen: HomeStack,
-    navigationOptions: {
-      tabBarLabel: 'Home',
+      headerLeft: null,
       tabBarIcon: ({ tintColor, focused }) => (
         <Icon name={focused ? "ios-home" : "ios-home-outline"} size={35} color={tintColor} />
       ),
     },
   },
-  FriendList: {
-    screen: FriendStack,
+  Friends: {
+    screen: FriendList,
     navigationOptions: {
-      tabBarLabel: 'Friends',
+      title: 'Search',
+      headerLeft: null,
       tabBarIcon: ({ tintColor, focused }) => (
-        <Icon name={focused ? "ios-contacts" : "ios-contacts-outline"} size={35} color={tintColor} />
+        <Icon name={focused ? "ios-search" : "ios-search-outline"} size={35} color={tintColor} />
       ),
     },
   },
-  NotificationList: {
-    screen: NotificationStack,
+  Notifications: {
+    screen: NotificationList,
     navigationOptions: {
-      tabBarLabel: 'Notifications',
+      title: 'Notifications',
+      headerLeft: null,
       tabBarIcon: ({ tintColor, focused }) => (
         <Icon name={focused ? "ios-notifications" : "ios-notifications-outline"} size={35} color={tintColor} />
       ),
     },
   },
-  MenuList: {
-    screen: MenuStack,
+  Menu: {
+    screen: MenuList,
     navigationOptions: {
-      tabBarLabel: 'Menu',
+      title: 'Menu',
+      headerLeft: null,
       tabBarIcon: ({ tintColor, focused }) => (
         <Icon name={focused ? "ios-menu" : "ios-menu-outline"} size={35} color={tintColor} />
       ),
     },
   },
-},  {
-  tabBarOptions: {
-    showLabel: false
-  },
+// },  {
+//   tabBarOptions: {
+//     showLabel: false
+//   },
 });
 
 // Root Stack containing SignUp and containing SignUp and TabNavigator
 export const Root = StackNavigator({
   SignUp: {
-    screen: SignUpStack,
+    screen: SignUp,
+    navigationOptions: {
+      title: 'Login',
+      headerLeft: null,
+    },
   }, Tabs: {
       screen: Tabs,
-  },
-}, {
-  mode: 'card',
-  headerMode: 'none',
-});
+  }, UserProfile: {
+      screen: UserProfile,
+      navigationOptions: ({ navigation }) => ({
+        title: `${toTitleCase(navigation.state.params.name.first)} ${toTitleCase(navigation.state.params.name.last)}`,
+      }),
+  }, Policy: {
+      screen: Policy,
+      navigationOptions: {
+        title: 'Privacy Policy',
+      },
+  }, Share: {
+      screen: ShareUpdate,
+      navigationOptions: {
+        title: 'Share',
+      },
+    },
+},);
