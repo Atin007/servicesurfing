c50f88085303b3cfcf58296d06a998ddc9b43749
Atin Mathur <atin.mathur.10@gmail.com>
2017-08-12 02:55:36 +0530

    Modified the about user screen

--- src/screens/AboutUser.js
+++ src/screens/AboutUser.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView, View, Text } from 'react-native';
+import { Card } from 'react-native-elements';
+import { toTitleCase } from '../helpers';
 
 class AboutUser extends Component {
 
   render() {
+    const user = this.props.navigation.state.params;
+
     return (
-      <View>
-        <Text>About</Text>
-      </View>
+      <ScrollView>
+        <Card>
+          <View>
+            <Text style={{fontSize: 15, fontWeight: 'bold', paddingBottom: 10}}>ABOUT</Text>
+            <Text style={{padding: 5}}>Name: {toTitleCase(user.name.first)} {toTitleCase(user.name.last)}</Text>
+            <Text style={{padding: 5}}>City: {toTitleCase(user.location.city)}</Text>
+            <Text style={{padding: 5}}>State: {toTitleCase(user.location.state)}</Text>
+            <Text style={{padding: 5}}>Projects: 0</Text>
+          </View>
+        </Card>
+        <Card>
+          <View>
+            <Text style={{fontSize: 15, fontWeight: 'bold', paddingBottom: 10}}>WORK</Text>
+            <Text style={{padding: 5}}>No Work Experience to display</Text>
+          </View>
+        </Card>
+        <Card>
+          <View>
+            <Text style={{fontSize: 15, fontWeight: 'bold', paddingBottom: 10}}>EDUCATION</Text>
+            <Text style={{padding: 5}}>No Educational qualifications to display</Text>
+          </View>
+        </Card>
+        <Card>
+          <View>
+            <Text style={{fontSize: 15, fontWeight: 'bold', paddingBottom: 10}}>PROJECTS</Text>
+            <Text style={{padding: 5}}>No Projects to display</Text>
+          </View>
+        </Card>
+      </ScrollView>
     );
   }
 
