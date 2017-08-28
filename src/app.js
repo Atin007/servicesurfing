841b86772bc9bc5e925a6d3a857987481f101dc1
atinm7 <atin.mathur.10@gmail.com>
2017-08-28 21:03:16 +0530

    Added the firebase package and added the signInEmail functionality

--- src/app.js
+++ src/app.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Root } from './config/router';
+import firebase from 'firebase';
 
-const App = () => {
-  return (
-      <Root />
-  );
+class App extends Component {
+  state = { loggedIn: null };
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyD4dw4Ydfn3OxAlwby9537udBK1c6_kMYU",
+      authDomain: "servicesurfing-e6cbc.firebaseapp.com",
+      databaseURL: "https://servicesurfing-e6cbc.firebaseio.com",
+      projectId: "servicesurfing-e6cbc",
+      storageBucket: "servicesurfing-e6cbc.appspot.com",
+      messagingSenderId: "389029720680"
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  render() {
+    return (
+        <Root />
+    );
+  }
 };
 
 export default App;
--- src/screens/SignInEmail.js
+++ src/screens/SignInEmail.js
@@ -12,9 +12,35 @@ import {
   FormInput,
   FormValidationMessage
 } from 'react-native-elements';
+import firebase from 'firebase';
 import { toTitleCase } from '../helpers';
 
 class SignInEmail extends Component {
+  state = { email: '', password: '', error: '', loading: false };
+
+  onButtonPress() {
+    const { email, password } = this.state;
+
+    this.setState({ error: '', loading: true });
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(this.onLoginSuccess.bind(this))
+      .catch(this.onLoginFail.bind(this));
+  }
+
+  onLoginFail() {
+    this.setState({ error: 'Incorrect username or password', loading: false });
+  }
+
+  onLoginSuccess() {
+    this.setState({
+      email: '',
+      password: '',
+      loading: false,
+      error: ''
+    });
+    this.props.navigation.navigate('Tabs');
+  }
 
   render() {
     return (
@@ -24,11 +50,24 @@ class SignInEmail extends Component {
       }}>
         <View style={{}}>
           <FormLabel>Email</FormLabel>
-          <FormInput placeholder="john.doe@wxyz.com" />
+          <FormInput
+            placeholder="john.doe@wxyz.com"
+            autoCorrect={false}
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
+          />
           <FormLabel>Password</FormLabel>
-          <FormInput placeholder="Password" />
-          <FormValidationMessage></FormValidationMessage>
-          <Button title="Login" onPress={() => console.log('hello')}/>
+          <FormInput
+            secureTextEntry={true}
+            placeholder="Password"
+            autoCorrect={false}
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+          />
+          <FormValidationMessage containerStyle={{alignItems: 'center'}}>
+              {this.state.error}
+          </FormValidationMessage>
+          <Button title="Login" onPress={this.onButtonPress.bind(this)}/>
         </View>
       </ScrollView>
     );
