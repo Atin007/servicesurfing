5d86e0fae683b4b9b1f1309964957d5cc4aac106
atinm7 <atin.mathur.10@gmail.com>
2017-09-25 16:50:50 +0530

    Modified Menu to use in house List component

--- src/screens/Menu.js
+++ src/screens/Menu.js
@@ -1,20 +1,17 @@
 import React, { Component } from 'react';
-import {
-  Text,
-  View,
-  ScrollView
-} from 'react-native';
+import { ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import { Icon } from 'react-native-elements';
+import { List, ListItem } from '../components/common';
 import firebase from 'firebase';
-import { List, ListItem } from 'react-native-elements';
-
-import { users, me } from '../config/data';
 
 class Menu extends Component {
+  constructor(props) {
+    super(props);
+  }
 
   componentWillMount() {
-
     const { currentUser } = firebase.auth();
-    this.list = [
+    this.MenuList = [
       {
         title: 'View Profile',
         icon: 'ios-contact',
@@ -25,37 +22,13 @@ class Menu extends Component {
         title: 'Friends',
         icon: 'ios-contacts',
         type: 'ionicon',
-        onPress: () => this.props.navigation.navigate('Friends', {...me})
+        onPress: () => this.props.navigation.navigate('Friends', {profileID: currentUser.uid})
       },
       {
         title: 'Appointments',
         icon: 'ios-calendar',
         type: 'ionicon',
-        onPress: () => this.props.navigation.navigate('Appointments', {...me})
-      },
-      {
-        title: 'Help Center',
-        icon: 'ios-help-circle',
-        type: 'ionicon',
-        onPress: null
-      },
-      {
-        title: 'Privacy Policy',
-        icon: 'ios-paper',
-        type: 'ionicon',
-        onPress: () => this.props.navigation.navigate('PrivacyPolicy')
-      },
-      {
-        title: 'Report a Problem',
-        icon: 'ios-bug',
-        type: 'ionicon',
-        onPress: null
-      },
-      {
-        title: 'About',
-        icon: 'ios-information-circle',
-        type: 'ionicon',
-        onPress: null
+        onPress: () => this.props.navigation.navigate('Appointments', {profileID: currentUser.uid})
       },
       {
         title: 'Logout',
@@ -67,21 +40,31 @@ class Menu extends Component {
   }
 
   render() {
+    const { textStyle } =styles;
     return (
       <ScrollView>
-        <List containerStyle={{marginTop: 0}}>
-          {this.list.map((item) => (
-            <ListItem
-              key={item.title}
-              title={item.title}
-              leftIcon={{name: item.icon, type: item.type, color: '#000'}}
-              onPress={item.onPress}
-            />
+        <List>
+          {this.MenuList.map((item, i) => (
+            <ListItem key={i}>
+              <Icon name={item.icon} type={item.type} size={22} color="#999" />
+              <TouchableOpacity onPress={item.onPress} activeOpacity={0.5}>
+                <Text style={textStyle}>{item.title}</Text>
+              </TouchableOpacity>
+            </ListItem>
           ))}
         </List>
       </ScrollView>
     );
   }
 }
+
+const styles = {
+  textStyle: {
+    alignSelf: 'center',
+    color:'#333',
+    fontSize: 16,
+    marginLeft: 20
+  }
+};
 
 export default Menu;
