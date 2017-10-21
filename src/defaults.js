0b11b299b9e55b32291909fa769ad7390f22175b
Atin Mathur <atin.mathur.10@gmail.com>
2017-10-21 15:43:25 +0530

    Modified DisplayPic and CoverPic urls

--- src/defaults.js
+++ src/defaults.js
@@ -1,6 +1,6 @@
-export const DEFAULT_DISPLAY_PIC = 'https://firebasestorage.googleapis.com/v0/b/servicesurfing-e6cbc.appspot.com/o/default-user.png?alt=media&token=899dcd9f-6951-4a61-b072-0818054a0840';
+export const DEFAULT_DISPLAY_PIC = 'https://firebasestorage.googleapis.com/v0/b/servicesurfing-d6831.appspot.com/o/sample_pics%2FDisplayPic.png?alt=media&token=90adbc25-e8ac-4de3-902b-25faeb8cba6f';
 
-export const DEFAULT_COVER_PIC = 'https://firebasestorage.googleapis.com/v0/b/servicesurfing-e6cbc.appspot.com/o/SS_default_cover.jpeg?alt=media&token=483b305d-7941-49e2-b92d-06441a30ef04';
+export const DEFAULT_COVER_PIC = 'https://firebasestorage.googleapis.com/v0/b/servicesurfing-d6831.appspot.com/o/sample_pics%2FCoverPic.jpeg?alt=media&token=9a1e3ed9-df4c-41c1-85e8-0e2360bf6a27';
 
 export const ToS = 'https://firebasestorage.googleapis.com/v0/b/servicesurfing-d6831.appspot.com/o/documents%2FToS.pdf?alt=media&token=6845e9f5-0084-4d8a-b779-57b36e0eefa6';
 
