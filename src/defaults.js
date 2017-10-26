ad52782d4207095b2e5c072281d57b4d530c0899
Atin Mathur <atin.mathur.10@gmail.com>
2017-10-26 15:19:18 +0530

    Modified Privacy Policy and Terms of Service Links

--- src/defaults.js
+++ src/defaults.js
@@ -2,6 +2,6 @@ export const DEFAULT_DISPLAY_PIC = 'https://firebasestorage.googleapis.com/v0/b/
 
 export const DEFAULT_COVER_PIC = 'https://firebasestorage.googleapis.com/v0/b/servicesurfing-d6831.appspot.com/o/sample_pics%2FCoverPic.jpeg?alt=media&token=9a1e3ed9-df4c-41c1-85e8-0e2360bf6a27';
 
-export const ToS = 'https://firebasestorage.googleapis.com/v0/b/servicesurfing-d6831.appspot.com/o/documents%2FToS.pdf?alt=media&token=6845e9f5-0084-4d8a-b779-57b36e0eefa6';
+export const ToS = 'https://firebasestorage.googleapis.com/v0/b/servicesurfing-d6831.appspot.com/o/documents%2FToS.pdf?alt=media&token=6928a4e5-d2af-45d7-8a6a-4ff1aaf676b1';
 
-export const PP = 'https://firebasestorage.googleapis.com/v0/b/servicesurfing-d6831.appspot.com/o/documents%2FPP.pdf?alt=media&token=ef8aae9f-792e-4187-8b80-f44b7fd079ed';
+export const PP = 'https://firebasestorage.googleapis.com/v0/b/servicesurfing-d6831.appspot.com/o/documents%2FPP.pdf?alt=media&token=442f7c60-b793-4992-a2a5-3e57b0647d74';
